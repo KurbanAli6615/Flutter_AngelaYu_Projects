2aba031572bfa5630d474b807ac41f93c0fa9854
KurbanAli6615 <kurabanalimasu@gmail.com>
2021-03-19 14:19:26 +0530

    flutter

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -12,6 +12,43 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  var leftDiceNumber = 1;
+  var rightDicenUmber = 4;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+              onPressed: () {},
+            ),
+          ),
+          Expanded(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              child: Image.asset("images/dice$rightDicenUmber.png"),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
