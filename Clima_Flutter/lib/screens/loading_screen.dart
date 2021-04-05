301d558603bfada0a004a0e5b18807cb269e852d
KurbanAli6615 <kurabanalimasu@gmail.com>
2021-04-05 17:18:41 +0530

    commit

--- Clima_Flutter/lib/screens/loading_screen.dart
+++ Clima_Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,22 +11,39 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=e734a23f0266dc3ba787c52670431563');
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      double temprature = jsonDecode(data)['main']['temp'];
+      int condition = jsonDecode(data)['weather'][0]['id'];
+      String cityName = jsonDecode(data)['name'];
+
+      print("Temprature : ${temprature}");
+      print("Condition : ${condition}");
+      print("cityName : ${cityName}");
+    } else {
+      print("Error in api response ${response.statusCode}");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
