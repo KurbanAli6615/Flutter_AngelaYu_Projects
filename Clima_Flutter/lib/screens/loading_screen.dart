f56ee9a6e7739c753b9345f4f32749b81878b2f0
KurbanAli6615 <kurabanalimasu@gmail.com>
2021-04-05 15:24:28 +0530

    Flutter

--- Clima_Flutter/lib/screens/loading_screen.dart
+++ Clima_Flutter/lib/screens/loading_screen.dart
@@ -10,6 +10,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
+    print(position);
   }
 
   @override
@@ -18,7 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
